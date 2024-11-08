e99ed6c1d26dd001b4ba987a87312fed0ca7c2de
Pastor <viruszold@gmail.com>
2024-11-08 16:50:13 +0300

    PVM

--- vol2/src/main/java/ru/mifi/practice/vol2/vm/Main.java
+++ vol2/src/main/java/ru/mifi/practice/vol2/vm/Main.java
@@ -6,12 +6,13 @@ public abstract class Main {
     public static void main(String[] args) throws IOException {
         VirtualMachine machine = new VirtualMachine.Default();
         String input = "1 10 - abs";
+        System.out.println("Input    : " + input);
         VirtualMachine.Binary binary = machine.compile(input);
-        System.out.println(binary);
+        System.out.println("Binary   : " + binary);
         VirtualMachine.Value compiled = machine.eval(binary, VirtualMachine.Context.newContext());
-        System.out.println(compiled);
+        System.out.println("Compiled : " + compiled);
         VirtualMachine.Value interpret = machine.eval(input, VirtualMachine.Context.newContext());
-        System.out.println(interpret);
+        System.out.println("Interpret: " + interpret);
 
     }
 }
