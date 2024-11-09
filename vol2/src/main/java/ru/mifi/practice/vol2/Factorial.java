e390ff2853143d998fda718906e1a1dac8f69bd3
Хлебников Андрей Александрович <andrey.khlebnikov@synchro.pro>
2024-11-09 18:48:40 +0300

    Polska VM

---
+++ vol2/src/main/java/ru/mifi/practice/vol2/Factorial.java
@@ -0,0 +1,25 @@
+package ru.mifi.practice.vol2;
+
+public final class Factorial {
+    private final int n;
+
+    public Factorial(int n) {
+        this.n = n;
+    }
+
+    private int recursiveFactorial(int n) {
+        return n <= 1 ? 1 : n * recursiveFactorial(n - 1);
+    }
+
+    public int iterationFactorial() {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+
+    public int recursiveFactorial() {
+        return recursiveFactorial(n);
+    }
+}
