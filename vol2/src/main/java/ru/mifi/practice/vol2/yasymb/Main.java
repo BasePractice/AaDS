a46cff053c205700dd3fcc1b354a21cca9298a0e
Хлебников Андрей Александрович <andrey.khlebnikov@synchro.pro>
2024-11-12 10:53:20 +0300

    YaSymbol

---
+++ vol2/src/main/java/ru/mifi/practice/vol2/yasymb/Main.java
@@ -0,0 +1,33 @@
+package ru.mifi.practice.vol2.yasymb;
+
+import java.util.Optional;
+
+public abstract class Main {
+
+    private static void start(String x, String y, String z) {
+        Optional<YaSymbol.Equation> processed = new YaSymbol.Simple().process(x, y, z);
+        if (processed.isPresent()) {
+            System.out.printf("------ %7d -------%n", processed.get().metrics().getOperations());
+        } else {
+            System.out.printf("----------------------%n");
+        }
+        new YaSymbol.Equation(x, y, z, new YaSymbol.Metrics()).print();
+        System.out.println();
+        if (processed.isPresent()) {
+            YaSymbol.Equation equation = processed.get();
+            equation.print();
+        } else {
+            System.out.println("UNSOLVED");
+        }
+        System.out.printf("----------------------%n");
+    }
+
+    public static void main(String[] args) {
+        start("x", "y", "z");
+        start("win", "lose", "game");
+        start("love", "hate", "feel");
+        start("four", "seven", "eight");
+        start("a", "b", "a");
+        start("odin", "odin", "mnogo");
+    }
+}
