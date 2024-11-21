23d917d58505ea7ca50862e38ed3b6574d1be7af
Хлебников Андрей Александрович <andrey.khlebnikov@synchro.pro>
2024-11-21 20:05:41 +0300

    Searching

--- vol3/src/main/java/ru/mifi/practice/vol3/BinaryNaturalSearch.java
+++ vol3/src/main/java/ru/mifi/practice/vol3/BinaryNaturalSearch.java
@@ -4,12 +4,21 @@ import java.util.Objects;
 import java.util.Optional;
 
 public final class BinaryNaturalSearch implements Search<Long, Number> {
+    private final boolean debug;
+
+    public BinaryNaturalSearch(boolean debug) {
+        this.debug = debug;
+    }
+
     @Override
     public Optional<Number> search(Long element, Range<Long> range, Function<Long> function, Counter counter) {
         var left = range.left;
         var right = range.right;
         while (!Objects.equals(left, right - 1)) {
             var mid = (right + left) >>> 1;
+            if (debug) {
+                System.out.printf("[%2d - %2d]. mid: %d%n", left, right, mid);
+            }
             var result = function.apply(mid);
             if (result > element) {
                 right = mid;
--- vol3/src/main/java/ru/mifi/practice/vol3/MainSearch.java
+++ vol3/src/main/java/ru/mifi/practice/vol3/MainSearch.java
@@ -6,19 +6,20 @@ import java.util.Optional;
 public abstract class MainSearch {
     public static void main(String[] args) {
         Counter counter = new Counter.Default();
-        Search<Long, Number> search = new BinaryNaturalSearch();
-        Optional<Number> n = search.search(24L, new Search.Range<>(10L, 20L), number -> 2 * number, counter);
+        boolean debug = true;
+        Search<Long, Number> search = new BinaryNaturalSearch(debug);
+        Optional<Number> n = search.search(24L, new Search.Range<>(10L, 17L), number -> 2 * number, counter);
         System.out.println("RES: " + n);
         System.out.println("OPS: " + counter);
         counter.reset();
-        var real = new BinaryRealSearch(0.0001);
-        n = real.search(24., new Search.Range<>(10., 20.), i -> 2 * i, counter);
+        var real = new BinaryRealSearch(0.0001, debug);
+        n = real.search(24., new Search.Range<>(10., 17.), i -> 2 * i, counter);
         System.out.println("RES: " + n);
         System.out.println("OPS: " + counter);
         counter.reset();
         List<Long> generated = NumberGenerator.generateSlice(1000);
         generated.sort(Long::compareTo);
-        var index = new BinaryIndexSearch(generated);
+        var index = new BinaryIndexSearch(generated, debug);
         n = index.search(10L, counter);
         System.out.println("RES: " + n);
         System.out.println("OPS: " + counter);
