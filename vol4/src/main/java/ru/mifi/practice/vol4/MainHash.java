3965e9d656e5a798e4e3d32f85a926ec75eb74a4
Pastor <viruszold@gmail.com>
2024-11-22 14:12:51 +0300

    Hash

---
+++ vol4/src/main/java/ru/mifi/practice/vol4/MainHash.java
@@ -0,0 +1,64 @@
+package ru.mifi.practice.vol4;
+
+import java.util.Date;
+import java.util.Random;
+import java.util.function.Function;
+
+public abstract class MainHash {
+    /**
+     * Origin  : text
+     * Hash    : 3556653
+     * Generate: tfYt
+     * Generate: v'yU
+     * Generate: uFxt
+     * Generate: v'xt
+     * Generate: uGZU
+     * Generate: teyU
+     * Generate: tg<6
+     * Generate: tf[6
+     * Hash    : 3556653
+     * @param origin оригинальный текст для которого необходимо подобрать хеш
+     * @param hash функция хеширования
+     */
+    private static void collision(String origin, Function<String, Integer> hash) {
+        Generator generator = new Generator();
+        int originHash = hash.apply(origin);
+        int generateHash = 0;
+        String generate = "";
+        int length = origin.length();
+        int it = 0;
+        while (originHash != generateHash) {
+            if (it > 10000000) {
+                it = 0;
+                length++;
+                System.out.println("Length  : " + length);
+            }
+            generate = generator.generate(length);
+            generateHash = hash.apply(generate);
+            ++it;
+        }
+        System.out.println("Origin  : " + origin);
+        System.out.println("Hash    : " + originHash);
+        System.out.println("Generate: " + generate);
+        System.out.println("Iterate : " + it);
+        System.out.println("Hash    : " + generateHash);
+    }
+
+    public static void main(String[] args) {
+        Hash hash = new Hash.DefaultHash();
+        collision("text", hash::hash);
+    }
+
+    private static final class Generator {
+        private final Random random = new Random(new Date().getTime());
+
+        public String generate(int length) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < length; i++) {
+                int symbol = random.nextInt(126 - 33) + 33;
+                sb.append((char) symbol);
+            }
+            return sb.toString();
+        }
+    }
+}
