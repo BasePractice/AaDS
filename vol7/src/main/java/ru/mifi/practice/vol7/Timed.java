5ce0cb24a2ba657b535c2401b71adaa177241c00
Pastor <viruszold@gmail.com>
2024-12-13 14:06:30 +0300

    Динамическое программирование(Рюкзак)

--- vol7/src/main/java/ru/mifi/practice/vol7/Timed.java
+++ vol7/src/main/java/ru/mifi/practice/vol7/Timed.java
@@ -1,19 +1,19 @@
 package ru.mifi.practice.vol7;
 
-import java.util.function.UnaryOperator;
+import java.util.function.Function;
 
 @FunctionalInterface
-public interface Timed<T> {
-    T timed(String title, T start, UnaryOperator<T> execution);
+public interface Timed<T, I> {
+    T timed(String title, I args, Function<I, T> execution);
 
 
-    final class Elapsed<T> implements Timed<T> {
+    final class Elapsed<T, I> implements Timed<T, I> {
 
         @Override
-        public T timed(String title, T init, UnaryOperator<T> execution) {
+        public T timed(String title, I args, Function<I, T> execution) {
             long start = System.currentTimeMillis();
             try {
-                return execution.apply(init);
+                return execution.apply(args);
             } finally {
                 long elapsed = System.currentTimeMillis() - start;
                 System.out.println(title + ": " + elapsed + "ms");
