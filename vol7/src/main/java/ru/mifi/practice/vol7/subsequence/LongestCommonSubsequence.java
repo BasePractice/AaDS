6fd133ee65e5931360af950f9531a69e482e97d5
Хлебников Андрей Александрович <andrey.khlebnikov@synchro.pro>
2024-12-13 19:12:53 +0300

    LCS

---
+++ vol7/src/main/java/ru/mifi/practice/vol7/subsequence/LongestCommonSubsequence.java
@@ -0,0 +1,29 @@
+package ru.mifi.practice.vol7.subsequence;
+
+import ru.mifi.practice.vol7.Counter;
+
+public interface LongestCommonSubsequence {
+    int longestCommonSubsequence(String text1, String text2, Counter counter);
+
+    final class Default implements LongestCommonSubsequence {
+
+        @Override
+        public int longestCommonSubsequence(String text1, String text2, Counter counter) {
+            int n = text1.length();
+            int m = text2.length();
+            int[][] table = new int[n + 1][m + 1];
+            for (int i = 1; i <= n; i++) {
+                counter.increment();
+                for (int j = 1; j <= m; j++) {
+                    counter.increment();
+                    if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                        table[i][j] = table[i - 1][j - 1] + 1;
+                    } else {
+                        table[i][j] = Math.max(table[i][j - 1], table[i - 1][j]);
+                    }
+                }
+            }
+            return table[n][m];
+        }
+    }
+}
