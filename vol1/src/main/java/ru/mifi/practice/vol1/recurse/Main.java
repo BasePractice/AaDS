d8dc1b1a64d5fdbb208c88ca9c990f8b125170af
Хлебников Андрей Александрович <andrey.khlebnikov@synchro.pro>
2025-11-12 11:05:40 +0300

    Реализация рекурсия

---
+++ vol1/src/main/java/ru/mifi/practice/vol1/recurse/Main.java
@@ -0,0 +1,19 @@
+package ru.mifi.practice.vol1.recurse;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public abstract class Main {
+    private static final AtomicLong CALLING =  new AtomicLong(0);
+    private static long fibonacci(int n) {
+        if (n < 2) {
+            return n;
+        }
+        CALLING.incrementAndGet();
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("FIB : " + fibonacci(50));
+        System.out.println("CALL: " + CALLING.get());
+    }
+}
