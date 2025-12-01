a22744db8607ee9f5fe4018aeca0d86b20658221
Хлебников Андрей Александрович <andrey.khlebnikov@synchro.pro>
2025-12-01 20:10:12 +0300

    Большое число при расчетах числа Фибоначчи

--- vol7/src/main/java/ru/mifi/practice/vol7/fibonacci/Fibonacci.java
+++ vol7/src/main/java/ru/mifi/practice/vol7/fibonacci/Fibonacci.java
@@ -2,21 +2,22 @@ package ru.mifi.practice.vol7.fibonacci;
 
 import ru.mifi.practice.vol7.Counter;
 
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 
 @FunctionalInterface
 public interface Fibonacci {
-    long fibonacci(int n, Counter counter);
+    BigInteger fibonacci(int n, Counter counter);
 
     final class Dynamited implements Fibonacci {
         @Override
-        public long fibonacci(int n, Counter counter) {
-            long value1 = 1;
-            long value2 = 1;
-            long value3 = 1;
+        public BigInteger fibonacci(int n, Counter counter) {
+            BigInteger value1 = BigInteger.ONE;
+            BigInteger value2 = BigInteger.ONE;
+            BigInteger value3 = BigInteger.ONE;
             for (int i = 2; i <= n; i++) {
-                value3 = value1 + value2;
+                value3 = value1.add(value2);
                 value1 = value2;
                 value2 = value3;
                 counter.increment();
@@ -26,33 +27,34 @@ public interface Fibonacci {
     }
 
     final class Memorized implements Fibonacci {
-        private static long fibonacci(int n, Map<Integer, Long> memo, Counter counter) {
+        private static BigInteger fibonacci(int n, Map<Integer, BigInteger> memo, Counter counter) {
             counter.increment();
             if (n == 0 || n == 1) {
-                return 1;
+                return BigInteger.ONE;
             } else if (memo.containsKey(n)) {
                 return memo.get(n);
             }
-            long left = fibonacci(n - 1, memo, counter);
-            long right = fibonacci(n - 2, memo, counter);
-            memo.put(n, left + right);
-            return left + right;
+            BigInteger left = fibonacci(n - 1, memo, counter);
+            BigInteger right = fibonacci(n - 2, memo, counter);
+            BigInteger added = left.add(right);
+            memo.put(n, added);
+            return added;
         }
 
         @Override
-        public long fibonacci(int n, Counter counter) {
+        public BigInteger fibonacci(int n, Counter counter) {
             return fibonacci(n, new HashMap<>(), counter);
         }
     }
 
     final class Recursion implements Fibonacci {
         @Override
-        public long fibonacci(int n, Counter counter) {
+        public BigInteger fibonacci(int n, Counter counter) {
             counter.increment();
             if (n == 0 || n == 1) {
-                return 1;
+                return BigInteger.ONE;
             }
-            return fibonacci(n - 1, counter) + fibonacci(n - 2, counter);
+            return fibonacci(n - 1, counter).add(fibonacci(n - 2, counter));
         }
     }
 }
