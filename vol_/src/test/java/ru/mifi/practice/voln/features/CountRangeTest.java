5f497d5345fee23b9faf8ddd30304537b3c79b63
Хлебников Андрей Александрович <andrey.khlebnikov@synchro.pro>
2025-12-03 14:14:25 +0300

    Счетчик. Добавлен индекс принятого значения

--- vol_/src/test/java/ru/mifi/practice/voln/features/CountRangeTest.java
+++ vol_/src/test/java/ru/mifi/practice/voln/features/CountRangeTest.java
@@ -19,18 +19,35 @@ class CountRangeTest {
     @BeforeEach
     void setUp() {
         count = new CountRange.Default();
-        countId = count.addCount("3", new long[]{0, 100, 200, 300}, new long[]{0, 0, 1, 2});
+        countId = count.addCount("3", new long[]{0, 100, 200, 300}, new long[]{0, 0, 1001, 1002});
     }
 
     @Test
     @DisplayName("Входит в диапазон")
     void inRange() {
+        count.increment(countId, USER_ID, 1);
+        checkCountValue(0L);
+        checkUserValue(1L);
+        assertTrue(count.availableValue(countId, USER_ID));
+        count.acceptValue(countId, USER_ID);
+        assertFalse(count.availableValue(countId, USER_ID));
+        count.increment(countId, USER_ID, 99);
+        checkCountValue(0L);
+        checkUserValue(100L);
+        count.increment(countId, USER_ID, 1);
+        checkCountValue(1001L);
+        checkUserValue(101L);
+    }
+
+    private void checkUserValue(long expected) {
+        Optional<Long> usered = count.userValue(countId, USER_ID);
+        assertTrue(usered.isPresent());
+        assertEquals(expected, usered.get().longValue());
+    }
+
+    private void checkCountValue(long expected) {
         Optional<Long> counted = count.countValue(countId, USER_ID);
-        assertTrue(counted.isEmpty());
-        Optional<Long> add = count.add(countId, USER_ID, 1);
-        assertFalse(add.isPresent());
-        add = count.add(countId, USER_ID, 99);
-        assertTrue(add.isPresent());
-        assertEquals(1L, add.get().longValue());
+        assertTrue(counted.isPresent());
+        assertEquals(expected, counted.get().longValue());
     }
 }
