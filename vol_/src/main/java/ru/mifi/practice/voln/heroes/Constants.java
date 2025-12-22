4048cb701823fa5b412d139b30dc3daa01262f41
Хлебников Андрей Александрович <andrey.khlebnikov@synchro.pro>
2025-12-22 16:12:09 +0300

    Heroes

---
+++ vol_/src/main/java/ru/mifi/practice/voln/heroes/Constants.java
@@ -0,0 +1,10 @@
+package ru.mifi.practice.voln.heroes;
+
+public final class Constants {
+    public static final int ROWS = 11;
+    public static final int COLS = 15;
+    public static final int CELL_SIZE = 50;
+
+    private Constants() {
+    }
+}
