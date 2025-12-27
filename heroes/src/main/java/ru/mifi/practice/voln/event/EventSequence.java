3d092a35e67084d2f9d771846c3a41fd0c25724b
Хлебников Андрей Александрович <andrey.khlebnikov@synchro.pro>
2025-12-28 02:03:28 +0300

    Добавление EventStream как хранителя событий

---
+++ heroes/src/main/java/ru/mifi/practice/voln/event/EventSequence.java
@@ -0,0 +1,16 @@
+package ru.mifi.practice.voln.event;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public interface EventSequence {
+    long next();
+
+    final class Default implements EventSequence {
+        private final AtomicLong counter = new AtomicLong(0);
+
+        @Override
+        public long next() {
+            return counter.incrementAndGet();
+        }
+    }
+}
